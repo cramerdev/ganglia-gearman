750e4b1246fb2f5957995ef9148bd017b11eba42
Nathan L Smith <nlloyds@gmail.com>
2012-08-31 12:00:10 -0500

    set up classes

---
+++ lib/ganglia/gearman/ganglia_connection.rb
@@ -0,0 +1,13 @@
+module Ganglia
+  class Gearman
+    class GangliaConnection
+      def initialize(host = '127.0.0.1', port = 8649)
+
+      end
+
+      def report(data)
+        true
+      end
+    end
+  end
+end
---
+++ lib/ganglia/gearman/gearman_server.rb
@@ -0,0 +1,13 @@
+module Ganglia
+  class Gearman
+    class GearmanServer
+      def initialize(host = '127.0.0.1', port = 4730)
+
+      end
+
+      def status
+
+      end
+    end
+  end
+end
